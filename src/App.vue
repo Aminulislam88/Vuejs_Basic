eceaf7ed4f2ffd3691346f8a2dae5b15e55be07d
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-14 00:29:32 +0600

    one image added

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,23 @@
 <script setup>
-import {data} from './Data/data.js'
-// const messeage="Welcome. Get ready to master Vue.js 3!"
-// const date="30th June, 2023"
-// const data={
-// messeage: "Welcome. Get ready to master Vue.js 3!",
-//     date: "13th july 2023",
-//     value:[1,2,3,4]
-// }
-
-//
-// const arrdata=[
-//   {
-//     messeage: "Welcome. Get ready to master Vue.js 3!",
-//     date: "13th july 2023",
-//   },
-//   {
-//     value:[1,2,3,4]
-//   }
-//
-// ]
-
-// function getDate(){
-//   return "7th july 2023"
-// }
-// function oddOrEven(n){
-//   if (n%2==0){
-//     return "Even"
-//   }
-//   return "odd"
-// }
-// function letDate(){
-//   const date=new Date();
-//   return date.toDateString();
-// }
-// const number=15
+const title="Water surrounded by trees and mountains"
+const image="//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+const link="https://unsplash.com/photos/NRQV-hBF10M"
 </script>
 
 <template>
-<div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-  <h1 class="text-4xl">{{data.messeage}}</h1>
-<!--  <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{date}}</span></h2>-->
-  <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{data.date}}</span></h2>
-<!--  <h2 class="mt-10 text-xl text-gray-700">Number is <span> {{oddOrEven(5)}}</span></h2>-->
-<!--  <h2 class="mt-10 text-xl text-gray-700">{{number}}  is {{number%2==0 ? "even" : "odd"}}</h2>-->
-<!--  <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{arrdata[1].value[2]}}</span></h2>-->
-</div>
+  <section class="container mx-auto p-10">
+    <h1 class="text-4xl mb-10">Data Project</h1>
+    <section class="main flex space-x-4">
+      <div class="flex space-y-4 flex-col w-1/2">
+        <img :title="title" :src="image" alt="">
+        <h2 class="text-xl">{{ title }}</h2>
+        <a target="_blank" :href="link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
+          Image Source
+        </a>
+      </div>
+    </section>
+  </section>
+
 </template>
 
 <style scoped>
