6e2d7b9f5cd3bc767566ec2620d4ed89e741f32b
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-07-14 17:12:59 +0600

    3rd logic done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 const location="Dhaka"
-const weather="r"
+const weather="pc"
 </script>
 
 <template>
@@ -9,14 +9,27 @@ const weather="r"
         <h1 class="text-center text-2xl py-10">Logic</h1>
         <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
             <h1 class="text-3xl">{{location}}</h1>
-            <h2 class="text-2xl" v-show="weather=='t'">Weather: thunderstrom</h2>
-            <h2 class="text-2xl" v-show="weather=='pc'">Weather: Partly Cloudy</h2>
-            <h2 class="text-2xl" v-show="weather=='s'">Weather: Sunny</h2>
-            <h2 class="text-2xl" v-show="weather=='r'">Weather: Rainy</h2>
-            <img v-show="weather=='s'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
-            <img v-show="weather=='pc'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
-            <img v-show="weather=='r'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
-            <img v-show="weather=='t'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
+
+            
+            <template v-if="weather=='t'">
+            <h2 class="text-2xl">Weather: thunderstrom</h2>
+            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
+            </template>
+
+            <template v-if="weather=='pc'">
+            <h2 class="text-2xl" >Weather: Partly Cloudy</h2>
+            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
+            </template>
+
+            <template v-if="weather=='s'">
+            <h2 class="text-2xl" >Weather: Sunny</h2>
+            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
+            </template>
+
+            <template v-if="weather=='r'">
+            <h2 class="text-2xl" >Weather: Rainy</h2>
+            <img  src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
+            </template>
         </div>
     </section>
 
